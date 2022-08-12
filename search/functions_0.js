e88d603231f8b91bfc924fee3672f694d3304a38
fjtrujy <fjtrujy@users.noreply.github.com>
2022-08-12 06:48:06 +0000

    deploy: bc6bd2f1c0ec4bf34f6da04768c602571d5fcd4f

---
+++ search/functions_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['_5f_5fattribute_5f_5f_0',['__attribute__',['../group__packet2__types.html#aa1044dde0d90113ab8c1d366fdb2a4e5',1,'packet2_t::__attribute__()'],['../libgs_8h.html#a72523af49266332290fbb373162839cc',1,'__attribute__((aligned(16))):&#160;libgs.h'],['../libmc_8c.html#a0f34140c15e0ab87056468c3df9576a6',1,'__attribute__((aligned(64))):&#160;libmc.c'],['../audsrv_8c.html#abd79b83ed512ceae97a0371c9b1e9c86',1,'__attribute__((aligned(64))):&#160;audsrv.c'],['../cdrom_8c.html#a06084e9218809d9aa1d7c218862fbdc4',1,'__attribute__((aligned(64))):&#160;cdrom.c']]],
+  ['_5fheap_5fmem_5ffit_1',['_heap_mem_fit',['../alloc_8c.html#a5ca7c518e68b8573893f7d253401aa59',1,'alloc.c']]],
+  ['_5fsifloadmodule_2',['_SifLoadModule',['../group__loadfile.html#gaa4e5b6e34b3523b9319dbae710682b62',1,'loadfile.h']]],
+  ['_5fstart_3',['_start',['../devfs_8c.html#a7d259b9f2c7d45f4713e0c483d0fa25b',1,'_start(int argc, char *argv[]):&#160;devfs.c'],['../group__fakehost.html#ga7d259b9f2c7d45f4713e0c483d0fa25b',1,'_start(int argc, char *argv[]):&#160;fakehost.c'],['../group__ps2netfs.html#ga7d259b9f2c7d45f4713e0c483d0fa25b',1,'_start(int argc, char *argv[]):&#160;ps2netfs.c'],['../audsrv_8c.html#a7d259b9f2c7d45f4713e0c483d0fa25b',1,'_start(int argc, char *argv[]):&#160;audsrv.c'],['../group__iopmgr.html#ga7d259b9f2c7d45f4713e0c483d0fa25b',1,'_start(int argc, char *argv[]):&#160;main.c']]]
+];
